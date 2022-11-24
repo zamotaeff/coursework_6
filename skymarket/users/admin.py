d1de696dae478f52a62249bfd305f614cfc48c68
zamotaev.v-a@yandex.ru <zamotaev.v-a@yandex.ru>
2022-11-25 01:43:35 +0300

    User admin view

--- skymarket/users/admin.py
+++ skymarket/users/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 
 from users.models import User
-# TODO Aдмика для пользователя - как реализовать ее можно подсмотреть в документаци django
-# TODO Обычно её всегда оформляют, но в текущей задачи делать её не обязательно
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'role', 'id')
+    list_display_links = ('email',)
+    list_filter = ('role',)
+    list_editable = ('role',)
+    search_fields = ('first_name', 'last_name', 'email')
