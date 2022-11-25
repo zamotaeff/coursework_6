16d455ed841b883ddde8c8bdd6a0a26a076937ec
zamotaev.v-a@yandex.ru <zamotaev.v-a@yandex.ru>
2022-11-25 04:50:53 +0300

    Add default permissions for ad model

--- skymarket/ads/permissions.py
+++ skymarket/ads/permissions.py
@@ -1,1 +1,21 @@
-# TODO здесь производится настройка пермишенов для нашего проекта
+from rest_framework.permissions import BasePermission
+
+from users.models import UserRoles
+
+
+class IsOwnerSelection(BasePermission):
+    message = "You are not the owner"
+
+    def has_object_permission(self, request, view, obj):
+        if request.user == obj.owner:
+            return True
+        return False
+
+
+class IsOwnerAdOrStaff(BasePermission):
+    message = "You are not the owner or admin"
+
+    def has_object_permission(self, request, view, obj):
+        if request.user == obj.author or request.user.role in [UserRoles.ADMIN, UserRoles.MODERATOR]:
+            return True
+        return False
