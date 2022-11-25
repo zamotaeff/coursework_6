4e20e8b19b67ec30c38c46f5162bf0991756f30a
zamotaev.v-a@yandex.ru <zamotaev.v-a@yandex.ru>
2022-11-25 04:06:19 +0300

    Ads admin view

--- skymarket/ads/admin.py
+++ skymarket/ads/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 
 from ads.models import Ad, Comment
 
-# TODO здесь можно подкючить ваши модели к стандартной джанго-админке
 
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'author', 'id')
+    list_display_links = ('title',)
+    list_filter = ('author', )
+    list_editable = ('price', )
+    search_fields = ('title',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('ad', 'author', 'id')
+    list_filter = ('ad', 'author',)
+    search_fields = ('author',)
