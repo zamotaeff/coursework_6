64e5d63dfd67db003ae6ad3855c0d985ab6e673a
zamotaev.v-a@yandex.ru <zamotaev.v-a@yandex.ru>
2022-11-27 09:11:42 +0300

    Add routes for create and refresh token

--- skymarket/users/urls.py
+++ skymarket/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path, re_path
 
 from djoser.views import UserViewSet
 from rest_framework.routers import SimpleRouter
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = SimpleRouter()
 router.register('users', UserViewSet, basename="users")
@@ -12,4 +12,7 @@ urlpatterns = [
     path("", include(router.urls)),
     re_path(r'^auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.jwt')),
+
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
 ]
